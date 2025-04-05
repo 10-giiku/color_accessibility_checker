16bfd6fae18387d618874145a296648a6cc10516
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-04-05 20:02:19 +0900

    ESlintのエラーを解消

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import Link from 'next/link';
 
 export default function Header() {
-    return (
-        <header className="flex items-center justify-between p-4 bg-gray-800 text-white "style={{ backgroundColor: 'rgb(30, 100, 175)' }}>
-        <h1 className="text-2xl font-bold"><Link href="/home">Tone Assist</Link></h1>
-        <nav>
-            <ul className="flex space-x-4">
-            <li> <a href="/mypage" className="hover:underline">Mypage</a></li>
-            </ul>
-        </nav>
-        </header>
-    );
+  return (
+    <header
+      className="flex items-center justify-between p-4 bg-gray-800 text-white"
+      style={{ backgroundColor: 'rgb(30, 100, 175)' }}
+    >
+      <h1 className="text-2xl font-bold">
+        <Link href="/home">Tone Assist</Link>
+      </h1>
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/mypage" className="hover:underline">
+              Mypage
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
