9f9e229165d344b26b39cdc1efabc903c00ec7bf
rii_3112 <aMatsu20191121@gmail.com>
2025-04-06 13:03:33 +0900

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -68,7 +68,7 @@ export default function Home() {
                     </button>
                 </form>
 
-              <> </div>  )}
+             </div>  <>  )}
            
         </div>
         <Footer className="mt-auto"/>
